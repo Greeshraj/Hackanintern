72fe135937c9e91102c4188e6815423fc9b96f04
Sumit Kumar Sharma <sksharma372@kgpian.iitkgp.ac.in>
2022-10-29 17:57:53 +0530

    done

--- stock_market/src/Components/BuySell/BuySell.js
+++ stock_market/src/Components/BuySell/BuySell.js
@@ -16,18 +16,39 @@ import "./BuySell.css"
 export default function BuySell() {
   
   const [value, setValue] = useState("");
+  const [buy_sell, setBuy_sell] = useState("");
+  const [user, setUser] = useState('');
+  const [limar, setLimar] = useState('');
+  const [amount, setAmount] = useState('')
+  const [price, setPrice] = useState('')
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const info = {buy_sell,user,limar,amount,price};
+
+    fetch('http://localhost:5000/post', {
+      method: 'POST',
+      headers : {"Content-Type": "application/json"},
+      body: JSON.stringify(info)
+    })
+    .then(() => {
+      console.log('added')
+    })
+  }
+
+
+
   return (
    <div className="buysell">
      <div>
       <h2>Stock Form</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <div class="bsupper">
           <label className="lebels">
             
-            <select value={value} onChange={(e)=>{
-              setValue(e.value)
+            <select name = "buy_sell" value={buy_sell} onChange={(e)=>{
+              setBuy_sell(e.value)
             }}>
               <option value="buy">BUY</option>
               <option value="shell">SELL</option>
@@ -36,23 +57,23 @@ export default function BuySell() {
           </label>
           <label className="lebels">
             
-            <select value={value} onChange={(e)=>{
-              setValue(e.value)
+            <select name='user' value={user} onChange={(e)=>{
+              setUser(e.value)
             }}>
               
-              <option value="user1">USER1</option>
-              <option value="user2">USER2</option>
-              <option value="user3">USER3</option>
-              <option value="user4">USER4</option>
-              <option value="user5">USER5</option>
-              <option value="user6">USER6</option>
+              <option value="User A">User A</option>
+              <option value="User B">User B</option>
+              <option value="User C">User C</option>
+              <option value="User D">User D</option>
+              <option value="User E">User E</option>
+              <option value="User F">User F</option>
               
             </select>
           </label>
           <label className="lebels">
             
-            <select value={value} onChange={(e)=>{
-              setValue(e.value)
+            <select name="limar" value={limar} onChange={(e)=>{
+              setLimar(e.value)
             }}>
               <option value="limit">LIMIT</option>
               <option value="market">MARKET</option>
@@ -62,19 +83,19 @@ export default function BuySell() {
         </div>
         <label className="labels">
           Stock AMOUNT 
-          <input value={value} onChange={(e)=>{
-            setValue(e.value)
+          <input name='amount' value={amount} onChange={(e)=>{
+            setAmount(e.value)
           }}/>
         </label>
         <label className="labels">
          AT PRICE 
-          <input value={value} onChange={(e)=>{
-            setValue(e.value)
+          <input name='price' value={price} onChange={(e)=>{
+            setPrice(e.value)
           }}/>
         </label>
 
         <div class="submit">
-          <button>Place Order</button>
+          <button name='submit'>Place Order</button>
         </div>
 
       
